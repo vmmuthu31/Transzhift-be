6359135b26624d82963dd5c055bb8c1d22e14263
Vairamuthu <mvairamuthu2003@gmail.com>
2024-12-23 21:36:36 +0530

    updated the company and transporter module

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ mongoose.connect(process.env.MONGO_URI);
 
 const transporterRoutes = require("./routes/transporter");
 const driverRoutes = require("./routes/driver");
+const jobRoutes = require("./routes/job");
 
 app.use("/api/transporter", transporterRoutes);
 app.use("/api/driver", driverRoutes);
+app.use("/api/job", jobRoutes);
 
 const PORT = 8000;
 app.listen(PORT, () => {
